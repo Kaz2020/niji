5a47fa683ac34bce579910cfc7ffd4ad1e36da0c
Kaz2020 <75407387+Kaz2020@users.noreply.github.com>
2020-12-03 18:07:02 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+ScrollReveal().reveal('.fainUe', {
+                duration: 1500,
+                delay: 500,
+                reset: false,
+                viewFactor: 0.4,
+            });
+
+            ScrollReveal().reveal('.fainShita', {
+                duration: 1500,
+                delay: 800,
+                reset: false,
+                viewFactor: 0.4,
+            });
+
+            ScrollReveal().reveal('.fain1', {
+                duration: 5000,
+                delay: 1100,
+                reset: false,
+                viewFactor: 0.4,
+            });
+
+            ScrollReveal().reveal('.top', {
+                duration: 2000,
+                origin: 'top',
+                delay: 100,
+                distance: '100px',
+                reset: true,
+                viewFactor: 0.4,
+            });
+            ScrollReveal().reveal('.right', {
+                duration: 2000,
+                origin: 'right',
+                delay: 100,
+                distance: '500px',
+                reset: true,
+                viewFactor: 0.4,
+            });
+
